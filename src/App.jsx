20123d056d37f7fa48348dbabbf7532d0ac76bdf
Mehul Mohan <mehulmpt@gmail.com>
2020-11-16 21:11:27 +0530

    default files added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import data from './data'
 import List from './List'
 function App() {
+	// Edit your return statement here to match the instructions
 	return <h2>reminder project setup</h2>
 }
 
