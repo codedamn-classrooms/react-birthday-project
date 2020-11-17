26c1eba1e6934a62038d069d6b7b3d6608f4bad9
Mehul Mohan <mehulmpt@gmail.com>
2020-11-17 20:25:59 +0530

    default files added

--- src/List.jsx
+++ src/List.jsx
@@ -3,9 +3,19 @@ import React from 'react'
 // accept props here
 const List = (props) => {
 	const { people } = props
-	// Map over prop "people" and display only the names in any way you like
-	return people.map((item) => {
-		return <p key={item.id}>{item.name}</p>
+	// Map over prop "people" and code the right structure
+
+	return people.map((person) => {
+		const { id, name, age, image } = person
+		return (
+			<article key={id} className="person">
+				<img src={image} alt={name} />
+				<div>
+					<h4>{name}</h4>
+					<p>{age} years</p>
+				</div>
+			</article>
+		)
 	})
 }
 
