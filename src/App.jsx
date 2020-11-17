bb362664dab24347752bfae913f9d7a0a6854902
Mehul Mohan <mehulmpt@gmail.com>
2020-11-17 22:50:49 +0530

    default files added

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import data from './data'
 import List from './List'
 function App() {
 	// create a state variable here
+	const [people, setPeople] = useState(data)
 
 	// this should clear all records
 	function clearAllRecords() {
-		
+		setPeople([])
 	}
 	return (
 		<main>
 			<section className="container">
-				{/* change this to state variable data */}
-				<h3>{[].length} birthdays today</h3>
-				<List people={[]} />
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />
 				<button onClick={clearAllRecords}>Clear All</button>
 			</section>
 		</main>
