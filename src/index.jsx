2dc5ed1c62ec47089a382e08fd785960aa769395
Mehul Mohan <mehulmpt@gmail.com>
2021-08-17 00:22:53 +0530

    lab

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import './index.css'
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
