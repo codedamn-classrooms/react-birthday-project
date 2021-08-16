a01f4ec9dcd65d646945059808f126f1dd24dff5
Mehul Mohan <mehulmpt@gmail.com>
2021-08-17 00:26:56 +0530

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import React, { useState } from 'react'
 import data from './data'
 import List from './List'
 function App() {
-	return <h2>reminder project setup</h2>
+	return (
+		<main>
+			<section className="container">
+				{/* Make change to "0" here */}
+				<h3>0 birthdays today</h3>
+				{/* pass data to list component */}
+				<List />
+			</section>
+		</main>
+	)
 }
 
 export default App
