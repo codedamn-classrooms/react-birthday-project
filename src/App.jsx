ec9e646738f403121acfc86b4a9204033789c919
Mehul Mohan <mehulmpt@gmail.com>
2021-08-17 00:29:22 +0530

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React, { useState } from 'react'
 import data from './data'
 import List from './List'
 function App() {
+	// create a state variable here
+
+	// this should clear all records
+	function clearAllRecords() {}
 	return (
 		<main>
 			<section className="container">
-				{/* Make change to "0" here */}
-				<h3>{data.length} birthdays today</h3>
-				{/* pass data to list component */}
-				<List people={data} />
+				{/* change this to state variable data */}
+				<h3>{[].length} birthdays today</h3>
+				<List people={[]} />
+				<button onClick={clearAllRecords}>Clear All</button>
 			</section>
 		</main>
 	)
--- src/List.jsx
+++ src/List.jsx
@@ -7,7 +7,15 @@ const List = (props) => {
 
 	return people.map((person) => {
 		const { id, name, age, image } = person
-		return <article />
+		return (
+			<article key={id} className="person">
+				<img src={image} alt={name} />
+				<div>
+					<h4>{name}</h4>
+					<p>{age} years</p>
+				</div>
+			</article>
+		)
 	})
 }
 
